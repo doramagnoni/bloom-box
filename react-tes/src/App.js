9ceb6a819213fe1d257da9a7160fc5c1c4b1c654
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-05-22 18:39:51 +0200

    bla bla bla

--- react-tes/src/App.js
+++ react-tes/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"; // Make sure you import your custom CSS file AFTER Bootstrap's CSS
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="primary">Test Bootstrap Button</Button>
     </div>
   );
 }
